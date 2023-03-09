1bd13a38018b54e6e3a5aa8f24e02d377acd874b
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-09 10:31:17 +0800

    setup dalleRoutes api configuration

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -6,5 +6,14 @@ dotenv.config();
 
 const router = express.Router();
 
-export default router;
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
 
+const openai = new OpenAIApi(configuration);
+
+router.route('/').get((req, res) => {
+    res.send('Hello from DALL-E!');
+});
+
+export default router;
