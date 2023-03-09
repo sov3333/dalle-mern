e9dec8da52e9fbc532ade580a144471da4e1f9aa
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-09 11:16:20 +0800

    implement postRoute, setup cloudinary, show images

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Loader, Card, FormField } from '../components';
 
 const RenderCards = ({ data, title }) => {
   if (data?.length > 0) {
-    return data.map((post) => <Card key={post.id} {...post} />)
+    return data.map((post) => <Card key={post._id} {...post} />)
   }
 
   return (
@@ -16,6 +16,30 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+        if (response.ok) {
+          const result = await response.json();
+          setAllPosts(result.data.reverse()); // show latest post on top
+        }
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchPosts();
+  }, []);
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -44,7 +68,7 @@ const Home = () => {
               />
             ) : (
               <RenderCards
-                data={[]}
+                data={allPosts}
                 title="No posts found"
               />
             )}
--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,3 +1,5 @@
+import FileSaver from 'file-saver';
+
 import { surpriseMePrompts } from '../constants';
 
 export function getRandomPrompt(prompt) {
@@ -10,4 +12,8 @@ export function getRandomPrompt(prompt) {
 
     // return the prompt
     return randomPrompt;
+}
+
+export async function downloadImage(_id, photo) {
+    FileSaver.saveAs(photo, `download-${_id}.jpg`);
 }
