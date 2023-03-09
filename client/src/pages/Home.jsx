c3c0c25f15f2a80e107f16c324ac61e7a6b0d77a
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-09 11:54:43 +0800

    update server prod url on client

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://dall-e-dc5m.onrender.com/api/v1/post', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
@@ -45,7 +45,7 @@ const Home = () => {
 
   const handleSearchChange = (e) => {
     clearTimeout(searchTimeout);
-    
+
     setSearchText(e.target.value);
 
     setSearchTimeout(
