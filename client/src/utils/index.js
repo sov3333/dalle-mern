33e9f9c0c457a6e655490152aa418b89bfc8ca87
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-09 09:56:57 +0800

    setup CreatePost, add utils, implement FormField

---
+++ client/src/utils/index.js
@@ -0,0 +1,13 @@
+import { surpriseMePrompts } from '../constants';
+
+export function getRandomPrompt(prompt) {
+    // get random prompt
+    const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+    const randomPrompt = surpriseMePrompts[randomIndex];
+
+    // if prompt is the same, re-run function
+    if (randomPrompt === prompt) return getRandomPrompt(prompt);
+
+    // return the prompt
+    return randomPrompt;
+}
