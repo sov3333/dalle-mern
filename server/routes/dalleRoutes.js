372786b15d7aae20e6c346daba3c6f56fffedb7b
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-09 10:49:59 +0800

    implement createPost img, fix dalleRoute img data

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -25,7 +25,7 @@ router.route('/').post(async (req, res) => {
             size: '1024x1024',
             response_format: 'b64_json',
         })
-        const image = aiResponse.data.data[0].image;
+        const image = aiResponse.data.data[0].b64_json;
         res.status(200).json({ photo: image });
     } catch (error) {
         console.log((error));
